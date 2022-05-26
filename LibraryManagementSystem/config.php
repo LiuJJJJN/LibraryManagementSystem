afedbe7c8588c2157588264b388a6537ecfe0840
ning <LiuJaNing@163.com>
2022-05-26 17:24:04 +0800

    重新打包、重新上传sql文件

--- LibraryManagementSystem/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-session_start();
-
-$conn = mysqli_connect("localhost", "root", "129807", "book");
-if ($conn == null)
-{
-    echo "数据库链接失败, 请查看修改 conn.php 中相关配置!";
-    die;
-}
--- LibraryManagementSystem/ly_check.php
+++
@@ -1,7 +0,0 @@
-<?php
-	require_once("config.php");
-	if($_SESSION["admin"]=="")
-	{
- 	echo "<script language=javascript>alert('请重新登陆！');window.location='admin_login.php'</script>";
-	}
-?>
